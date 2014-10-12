68d4d7fa6290ece817d0efd40f5e2b45dc7f75e2
e-koch <koch.eric.w@gmail.com>
2014-10-12 16:01:02 -0600

    Updates to autoflagging

--- vla_heracles/akl_21cm_autoflag.py
+++ vla_heracles/akl_21cm_autoflag.py
@@ -24,19 +24,18 @@ from taskinit import *
 import casac
 
 # my casa routines
-import casapy_util
-    
+# import casapy_util
+
 ##############################################
 ### Apply basic autoflagging to a data set ###
 ##############################################
 
-def autoflag_21cm(
-    out_root = None,
-    edge_str = None,
-    edge_chan = None,
-    reset=True,
-    old_version='imported'
-    ):
+
+def autoflag_21cm(out_root=None,
+                  edge_str=None,
+                  edge_chan=None,
+                  reset=True,
+                  old_version='imported'):
 
     ### Defaults and error checking ###
 
@@ -50,49 +49,50 @@ def autoflag_21cm(
 
     ### Define files ###
 
-    vis = out_root+'.ms'
+    vis = out_root + '.ms'
 
     ### Reset Flagging  ###
 
     if (reset == True):
-        print "... reseting flags to versionname = "+old_version        
+        print "... reseting flags to versionname = " + old_version
 
-        flagmanager(vis=vis,mode='restore',versionname=old_version)
+        flagmanager(vis=vis, mode='restore', versionname=old_version)
 
     ### 10s quack ###
 
-    print "... 10s at the beginning of each scan."       
-    flagdata(vis=vis,mode='quack', quackinterval=10)
+    print "... 10s at the beginning of each scan."
+    flagdata(vis=vis, mode='quack', quackinterval=10)
 
     ### Shadowing ###
 
-    print "... flagging shadowed data."       
-    flagdata(vis=vis,mode='shadow')
+    print "... flagging shadowed data."
+    flagdata(vis=vis, mode='shadow')
 
     ### Autocorrelations ###
 
-    print "... flagging autocorrelations"        
-    flagautocorr(vis=vis)
+    print "... flagging autocorrelations"
+    flagdata(vis=vis, autocorr=True)
 
     ### Edge Channels ###
 
-    # the user gives an explicit selection string        
+    # the user gives an explicit selection string
     if (edge_str != None):
         print "... flagging edge channels with user selection"
 
         flagdata(vis=vis, spw=edge_str, mode='manualflag',
-                 flagbackup=False, selectdata=True)
+                 flagbackup=False)
 
     # the user asks for # of "edge_chan" channels to be flagged
     if (edge_chan != None):
-        print "... flagging edge channels with automated selection"
+        pass
+        # print "... flagging edge channels with automated selection"
 
         # call my "edge_channel_selection" script
-        spw_string = casapy_util.edge_chan_selection(vis=vis
-                                                   , edge_chan=edge_chan)
-        
-        flagdata(vis=vis, spw=spw_string, mode='manualflag',
-                 flagbackup=False, selectdata=True)
+        # spw_string = casapy_util.edge_chan_selection(
+        #     vis=vis, edge_chan=edge_chan)
+
+        # flagdata(vis=vis, spw=spw_string, mode='manualflag',
+        #          flagbackup=False, selectdata=True)
 
     ### Save results ###
 
@@ -100,11 +100,9 @@ def autoflag_21cm(
                 versionname='autoflagged',
                 comment='flags after shadow, autocorr, edge.')
 
-
     ### Finish ###
-
     print "----------- autoflag_21cm ends -----------"
 
     # Done!
-    
+
     return
