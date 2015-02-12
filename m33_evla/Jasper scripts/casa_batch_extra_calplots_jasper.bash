c244eae691cc0102c120d6db85713665551e97af
e-koch <koch.eric.w@gmail.com>
2015-02-12 16:11:07 -0700

    Update bash script for plots to use the locally saved version.

--- m33_evla/Jasper scripts/casa_batch_extra_calplots_jasper.bash
+++ m33_evla/Jasper scripts/casa_batch_extra_calplots_jasper.bash
@@ -1,45 +1,33 @@
 #!/bin/bash
 
-# Grab the certificate
-echo Get certificate
-getCert
-
-# Make some temporary directories
-echo "Making dirs"
-mkdir -p ${TMPDIR}/{vos,vos_cache,proc,vos_link}
-
-# Mount VOSpace
-echo "Mount VOS in readonly mode"
-mountvofs --vospace vos:MWSynthesis/VLA/14B-088/ --mountpoint ${TMPDIR}/vos --ca$
-
 # Load in casa pointer
-echo Sourcing
-source /home/cloud-user/.bashrc
+echo 'Sourcing'
+source /home/ekoch/.bashrc
 
-# Copy the necessary code
+# Make sure we're on the right branch
+mv /home/ekoch/code_repos/hi_reduction_scripts
 git checkout EVLA_pipeline
-
-cp code_repos/hi_reduction_scripts/m33_evla/EVLA_pipeline1.3.0/* EVLA_pipeline1.3.0/
-cp code_repos/hi_reduction_scripts/m33_evla/spw_plots.py .
-
-cd ${TMPDIR}/proc
 
 # Specify MSfile
 ms_folder='14B-088_20141021_1413960928386/'
 ms_file='14B-088.sb29701604.eb29882607.56952.08797296297.ms'
 
-full_path=$ms_folder'products/'$ms_file
+
+cd /home/ekoch/m33/${ms_folder}'products/'
+
+# Copy the necessary code
+mkdir EVLA_pipeline1.3.0/
+cp /home/ekoch/code_repos/hi_reduction_scripts/m33_evla/EVLA_pipeline1.3.0/* EVLA_pipeline1.3.0/
+cp /home/ekoch/code_repos/hi_reduction_scripts/m33_evla/spw_plots.py .
 
 # Run the code
 echo "Run casapy and spw_plots.py"
-casapy --nogui --nologger -c spw_plots.py full_path
+/home/ekoch/casapy-42.2.30986-1-64b/casapy --nogui --nologger -c spw_plots.py ms_file
 
-# Unmount VOSpace and copy output back over.
-echo 'Unmount VOS'
-fusermount -zu ${TMPDIR}/vos
-echo 'Mount VOS'
-mountvofs --vospace vos:MWSynthesis/VLA/14B-088/$ms_folder --mountpoint ${TMPDIR}/vos --ca$
-echo 'Copy files to VOS'
-cp -rf ${TMPDIR}/proc/* ${TMPDIR}/vos/
-echo 'Unmount VOS'
-fusermount -zu ${TMPDIR}/vos
+mkdir spw_plots
+
+mv *.png spw_plots
+
+# Clean-up
+rm -rf EVLA_pipeline1.3.0
+rm spw_plots.py
